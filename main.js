d20172a4c2701f77b030c3e0346bc8651725ae20
cronugs <cronugs@outlook.com>
2019-04-17 22:28:16 +0200

    started adding comments to help understand the code better

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+//5. getData is called from the writeToDocument() function. It is called with the url and a callback function, which formats and renders 
+//data into a table.
 function getData(url, cb) {
+    //we set a new XMLHttpRequest to the variable xhr
     var xhr = new XMLHttpRequest();
 
+    //6. we set onredystatechange to an annon function which tests the status of the data from the API
     xhr.onreadystatechange = function() {
+        //7. if the ready state is 4 and the status is 200
         if (this.readyState == 4 && this.status == 200) {
+            //8. we call our callback function in the getData call and parse is as JSON data. The data held in this.responseText
+            //(xhr.responseText) is passed back into the the writeToDocument function.
             cb(JSON.parse(this.responseText));
         }
     };
-
+    //I'm not sure what these lines do!
     xhr.open("GET", url);
     xhr.send();
 }
@@ -32,13 +39,18 @@ function generatePaginationButtons(next, prev) {
     }
 }
 
+
+//1. this is where we enter after the button is pressed and the url is passed as a parameter into the writeToDocument function
 function writeToDocument(url) {
+    //2. we create an empty var tableRows to store our table
     var tableRows = [];
+    //3. we create a var el that refers to the div #data 
     var el = document.getElementById("data");
-
+    //4. we call getData, which triggers a callback that puts the JSON data into the data variable.
     getData(url, function(data) {
+        //we set pagination to an empty string
         var pagination = "";
-
+        //We will pick up here when we can.
         if (data.next || data.previous) {
             pagination = generatePaginationButtons(data.next, data.previous);
         }
