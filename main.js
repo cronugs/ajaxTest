a7b0426175e4798b0f1f24e95b6d104eeec3822f
Jimi Windmills <jimi.windmills@gmail.com>
2019-04-19 22:00:13 +0200

    added comments for code learnings

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getData(url, cb) {
     xhr.onreadystatechange = function() {
         //7. if the ready state is 4 and the status is 200
         if (this.readyState == 4 && this.status == 200) {
-            //8. we call our callback function in the getData call and parse is as JSON data. The data held in this.responseText
+            //8. we call our callback function in the getData call and parse it as JSON data. The data held in this.responseText
             //(xhr.responseText) is passed back into the the writeToDocument function.
             cb(JSON.parse(this.responseText));
         }
@@ -18,18 +18,28 @@ function getData(url, cb) {
     xhr.send();
 }
 
+//Continue from here
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
+    console.log(Object.keys(obj));
+    
+
     Object.keys(obj).forEach(function(key) {
+        console.log(key);
+        
         tableHeaders.push(`<td>${key}</td>`);
     });
 
     return `<tr>${tableHeaders}</tr>`;
 }
 
+//11. generatePaginationButtons is called from the getData function and takes two arguments, next and prev.
+//which will be either a url or null
 function generatePaginationButtons(next, prev) {
+    //12. if both next and prev are a url;
     if (next && prev) {
+        //13. return code to create both a next and prev button to more through pages of results.
         return `<button onclick="writeToDocument('${prev}')">Previous</button>
                 <button onclick="writeToDocument('${next}')">Next</button>`;
     } else if (next && !prev) {
@@ -48,14 +58,27 @@ function writeToDocument(url) {
     var el = document.getElementById("data");
     //4. we call getData, which triggers a callback that puts the JSON data into the data variable.
     getData(url, function(data) {
-        //we set pagination to an empty string
+        //9. we set pagination to an empty string
         var pagination = "";
-        //We will pick up here when we can.
+        //10. Our if statement returns true if either the next or previous values in out data are not null.
         if (data.next || data.previous) {
+            //11. if our if statement returns true, we create a variable; pagination and pass in out data.next
+            //and data.previous variable which are either a url, or null.
             pagination = generatePaginationButtons(data.next, data.previous);
         }
-        data = data.results;
+        //console.log(data.count);
+        //console.log(data.next);
+        //console.log(data.previous);
+        //console.log(data.results);
+        //14. data.results is the meat of our data. Here we assign it to the variable data.
+        data = data.results; 
+        console.log(data[0]);
+
+        //15. create a variable tableHeaders and assign it the return value of the getTableHeaders function
         var tableHeaders = getTableHeaders(data[0]);
+        
+        
+        
 
         data.forEach(function(item) {
             var dataRow = [];
